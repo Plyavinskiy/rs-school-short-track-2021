63812912691d112ff41b933e96dc9c4596b37790
Plyavinskiy <klerk2012@gmail.com>
2021-04-14 18:46:45 +0300

    feat: get encoded string

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let encodedStr = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] === str[i]) {
+      count++;
+    }
+
+    if (str[i + 1] !== str[i]) {
+      if (count > 1) {
+        encodedStr += `${count + str[i]}`;
+        count = 1;
+      } else {
+        encodedStr += str[i];
+      }
+    }
+  }
+
+  return encodedStr;
 }
 
 module.exports = encodeLine;
