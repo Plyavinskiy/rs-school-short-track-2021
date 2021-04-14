e57e1fac92dee90e7c0d623774f67e0e54328745
Plyavinskiy <klerk2012@gmail.com>
2021-04-14 14:29:30 +0300

    feat: get the required array index according to O (log n)

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,28 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let startIndex = 0;
+  let endIndex = array.length - 1;
+  let middleIndex;
+
+  while (startIndex <= endIndex) {
+    middleIndex = Math.floor((startIndex + endIndex) / 2);
+
+    if (array[middleIndex] === value) {
+      return middleIndex;
+    }
+
+    if (array[middleIndex] < value) {
+      startIndex = middleIndex + 1;
+    }
+
+    if (array[middleIndex] > value) {
+      endIndex = middleIndex - 1;
+    }
+  }
+
+  return -1;
 }
 
 module.exports = findIndex;
